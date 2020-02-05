ce946ce988fcf392e0492830e84fe3af92241693
hiren0ded <15bce077@nirmauni.ac.in>
2020-02-05 21:47:24 +0530

    Checking Build Pipeline

--- src/main/java/com/game/demo/controllers/MainController.java
+++ src/main/java/com/game/demo/controllers/MainController.java
@@ -6,10 +6,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
+    static int visit = 0;
 
     @GetMapping("/")
     public ModelAndView swagger()
     {
+        visit++;
+        System.out.println("Visit Count::" + visit);
         return new ModelAndView("redirect:" + "swagger-ui.html");
     }
 
